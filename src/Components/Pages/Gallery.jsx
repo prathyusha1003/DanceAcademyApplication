30c67b6d19169d88625ae17975798b2a72cec5a6
prathyusha1003 <prathyu1003@gmail.com>
2024-04-28 16:14:58 +0530

    DanceAcademyAppliction

---
+++ src/Components/Pages/Gallery.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Gallery = () => 
+{
+    return (
+        <div>
+            <h1 id='id1'><b><i>Our Speciality</i></b></h1>
+            <div id='doo1'>
+                <img id="im1" src="https://news.northwestern.edu/assets/Stories/2023/02/Screen-Shot-2020-03-31-at-10__FitMaxWzk3MCw2NTBd.52.11-AM.png" alt="" height="300px" width="380px"/>
+                <img id="im2" src="https://www.danceflavors.com/wp-content/uploads/2022/09/two-dancers-young-man-and-woman-dancing-hip-hop-i-2021-12-09-16-11-52-utc-scaled-e1681741492544.jpg" alt="" height="300px" width="380px"/>
+                <img id="im3" src="https://static1.squarespace.com/static/5d8526e4f73668483f296428/t/617b505814f53540d5215ee1/1635471448785/Gustavo%2BNaveira%2Band%2BGiselle%2BAnne%2Bin%2BBoston-3.jpg?format=1500w" alt="" height="300px" width="380px"/> 
+            </div>
+            <div id='doo2'>
+                <div id='id2'><b><i>Contemporary</i></b></div>
+                <div id='id3'><b><i>Hip Hop</i></b></div>
+                <div id='id4'><b><i>Salsa</i></b></div>
+            </div>
+            <br/><br/>
+            <div id='doo3'>
+                <img id="im4" src="https://assets.vogue.in/photos/5ce469df3bf9b4a67eb217ef/master/pass/Ballet_27-10-150302.jpg" alt="" height="300px" width="380px"/>
+                <img id="im5" src="https://newcastledanceacademy.com.au/wp-content/uploads/2019/12/thumb_600x600_2111590.jpg" alt="" height="300px" width="380px"/>
+                <img id="im6" src="https://www.yourdailydance.com/wp-content/uploads/2023/02/Jazz-Dance-.jpg" alt="" height="300px" width="380px"/>
+            </div>
+            <div id='doo4'>
+                <div id='id5'><b><i>Ballet</i></b></div>
+                <div id='id6'><b><i>Tap Dancing</i></b></div>
+                <div id='id7'><b><i>Jazz</i></b></div>
+            </div>
+        </div>
+    )
+}
+
+export default Gallery
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div id='do1'>
+                <img src="https://cdn.pixabay.com/photo/2013/11/03/08/05/cheers-204742_1280.jpg" alt="" height="600px" width="1226"/>
+            </div>    
+            <div>
+                <h1 id='we1'><b><i>" ...Enhance Your</i></b></h1>          
+                <h1 id='we2'><b><i>Passion For Dance...! "</i></b></h1>      
+            </div>    
+        </div>
+    )
+}
+
+export default Home
